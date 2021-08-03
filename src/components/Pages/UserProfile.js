6c0ce37f34f9a038f879c5094e7742243ffc1b8f
hrize <hrizexshahi@gmail.com>
2021-08-03 17:05:02 +0545

    Logged in profile is shown.

--- src/components/Pages/UserProfile.js
+++ src/components/Pages/UserProfile.js
@@ -1,98 +1,131 @@
-import React from 'react'
-import { Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import profile from "../../images/profile.png";
-import emptycart from "../../images/empty.png";
-
 
 const UserProfile = () => {
-    return (
-        <>
-            <div className="container emp-profile">
-                <form method="">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <img src={profile} alt="profile" />
-                        </div>
-
-                        <div className="col-md-6">
-                            <div className="profile-head">
-                                <h2>User Profile</h2>
-                                <h4>Customer</h4>
-
-                                <ul className="nav nav-tabs" role="tablist">
-                                    <li className="nav-item">
-                                        <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab">Dashboard</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab">Account Details</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link active" id="address-tab" data-toggle="tab" href="#address" role="tab">Address</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link active" id="wishlist-tab" data-toggle="tab" href="#wishlist" role="tab">Wishlist</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div className="col-md-2">
-                            <Link to="/editprofile" className="text-black-50"><Button variant="contained" color="primary">Edit Profile</Button></Link>
-                            {/* <input type ="submit" className="profile-edit-btn" name="btnAddMore" values="Edit Profile"/> */}
-
-                        </div>
-                    </div>
-
-                    {/* right side data toogle */}
-
-                    <div className="col-md-8 pl-5 dashboard-info">
-                        <div className="tab-content profile-tab" id="myTabContent">
-                            <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                <div className="row">
-                                    <div className="coml-md-6">
-                                        <label>Your cart is empty</label>
-                                        <img src={emptycart} alt="emptycart" />
-                                        {/* <Link to="/shop" className="text-black-50"></Link> */}
-                                        <Button variant="contained" color="primary">Shop Now!</Button>
-                                    </div>
-
-                                </div>
-
-                                <div className="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                    <div className="row">
-                                        <div className="coml-md-6">
-                                            <label>Account Details</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="tab-pane fade show active" id="address" role="tabpanel" aria-labelledby="address-tab">
-                                    <div className="row">
-                                        <div className="coml-md-6">
-                                            <label>Address</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="tab-pane fade show active" id="wishlist" role="tabpanel" aria-labelledby="wishlist-tab">
-                                    <div className="row">
-                                        <div className="coml-md-6">
-                                            <label>Wishlist</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <Button variant="contained" color="primary">Logout</Button>
-
-                </form>
+  return (
+    <>
+      <div className="container emp-profile">
+        <form method="">
+          <div className="row">
+            <div className="col-md-4 profile-head">
+              <img src={profile} alt="profile" />
+              <h2>User Profile</h2>
+              <h4>Customer</h4>
             </div>
-        </>
-    )
-}
 
-export default UserProfile
+            <div className="col-md-6">
+              <div className="profile-head">
+                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                  <li class="nav-item">
+                    <a
+                      class="nav-link active"
+                      id="pills-home-tab"
+                      data-toggle="pill"
+                      href="#pills-home"
+                      role="tab"
+                      aria-controls="pills-home"
+                      aria-selected="true"
+                    >
+                      Dashboard
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="pills-account-tab"
+                      data-toggle="pill"
+                      href="#pills-account"
+                      role="tab"
+                      aria-controls="pills-account"
+                      aria-selected="false"
+                    >
+                      Account Details
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="pills-address-tab"
+                      data-toggle="pill"
+                      href="#pills-address"
+                      role="tab"
+                      aria-controls="pills-address"
+                      aria-selected="false"
+                    >
+                      Address
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="pills-wishlist-tab"
+                      data-toggle="pill"
+                      href="#pills-wislist"
+                      role="tab"
+                      aria-controls="pills-wishlist"
+                      aria-selected="false"
+                    >
+                      Wishlist
+                    </a>
+                  </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                  <div
+                    class="tab-pane fade show active"
+                    id="pills-home"
+                    role="tabpanel"
+                    aria-labelledby="pills-home-tab"
+                  >
+                    Dashboard
+                  </div>
+                  <div
+                    class="tab-pane fade"
+                    id="pills-account"
+                    role="tabpanel"
+                    aria-labelledby="pills-account-tab"
+                  >
+                    ...
+                  </div>
+                  <div
+                    class="tab-pane fade"
+                    id="pills-address"
+                    role="tabpanel"
+                    aria-labelledby="pills-address-tab"
+                  >
+                    ...
+                  </div>
+                  <div
+                    class="tab-pane fade"
+                    id="pills-wishlist"
+                    role="tabpanel"
+                    aria-labelledby="pills-wishlist-tab"
+                  >
+                    ...
+                  </div>
+                </div>
+              </div>
+            </div>
 
+            <div className="col-md-2">
+              <Link to="/editprofile" className="text-black-50">
+                <Button variant="contained" color="primary">
+                  Edit Profile
+                </Button>
+              </Link>
+              {/* <input type ="submit" className="profile-edit-btn" name="btnAddMore" values="Edit Profile"/> */}
+              <Link to="/" className="text-black-50">
+                <Button variant="contained" color="primary">
+                  Logout
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default UserProfile;
