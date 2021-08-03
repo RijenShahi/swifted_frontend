a033daea10343863c9e30a2ba4a7e1f64e67f61a
hrize <hrizexshahi@gmail.com>
2021-08-03 17:10:40 +0545

    Navbar is setted as per user type.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/Pages/Home';
-import Navbar from './components/Frame/Navbar';
-import Navrbartop from './components/Frame/Navbartop';
-import NavItem from './components/Frame/NavItem';
-import Register from './components/Pages/Register';
-import Login from './components/Pages/Login';
-import UserProfile from './components/Pages/UserProfile';
-import EditProfile from './components/Pages/EditProfile';
-import Footer from './components/Frame/Footer';
-import AddProducts from './components/Pages/AddProducts';
-import About from './components/Pages/About';
-import Contact from './components/Pages/Contact';
-import Shop from './components/Pages/Shop';
-import MyCart from './components/Pages/MyCart';
-import Wishlist from './components/Pages/Wishlist';
-import Vendor from './components/Pages/Vendor';
-import UpdateProduct from './components/Pages/UpdateProduct';
-import RetrieveUpdateDelete from './components/Pages/RetrieveUpdateDelete'
-import VendorRequest from './components/Vendor/vendorRequest';
-import Checkout from './components/Pages/Checkout';
-import NavFull from './components/Navbar/NavFull';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Pages/Home";
+import Navbar from "./components/Frame/Navbar";
+import Navrbartop from "./components/Frame/Navbartop";
+import NavItem from "./components/Frame/NavItem";
+import Register from "./components/Pages/Register";
+import Login from "./components/Pages/Login";
+import UserProfile from "./components/Pages/UserProfile";
+import EditProfile from "./components/Pages/EditProfile";
+import Footer from "./components/Frame/Footer";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
+import Shop from "./components/Pages/Shop";
+import Cart from "./components/Pages/Cart";
+import Wishlist from "./components/Pages/Wishlist";
+import controlPanalProduct from "./components/Pages/ControlPanalProduct";
+import UpdateProduct from "./components/Pages/UpdateProduct";
+import RetrieveUpdateDelete from "./components/Pages/RetrieveUpdateDelete";
+import VendorRequest from "./components/Vendor/vendorRequest";
+import Checkout from "./components/Pages/Checkout";
+import NavFull from "./components/Navbar/NavFull";
+import CardsForCart from "./components/Cards/CardsForCart";
 
 function App() {
   return (
     <>
       <Router>
-        <NavFull/>
+        <NavFull />
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path="/" exact component={Home} />
           <Route path="/register" exact component={Register} />
           <Route path="/login" exact component={Login} />
           <Route path="/userprofile" exact component={UserProfile} />
           <Route path="/editprofile" exact component={EditProfile} />
-          <Route path="/addproduct" exact component={AddProducts} />
           <Route path="/updateProduct" exact component={UpdateProduct} />
           <Route path="/products" exact component={RetrieveUpdateDelete} />
           <Route path="/about" exact component={About} />
           <Route path="/contact" exact component={Contact} />
           <Route path="/shop" exact component={Shop} />
-          <Route path="/myCart" exact component={MyCart} />
+          <Route path="/myCart" exact component={Cart} />
           <Route path="/wishlist" exact component={Wishlist} />
-          <Route path="/vendor" exact component={Vendor} />
+          <Route path="/controlPanel" exact component={controlPanalProduct} />
           <Route path="/vendorRequest" exact component={VendorRequest} />
           <Route path="/checkout" exact component={Checkout} />
+          <Route path="/product/:pid" exact component={CardsForCart}></Route>
         </Switch>
-        <Footer/>
+        <Footer />
       </Router>
     </>
   );
--- src/components/Frame/Navbartop.js
+++ src/components/Frame/Navbartop.js
@@ -1,85 +1,84 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom';
-import Dropdown from '../Frame/Dropdown/AccountDropdown';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Dropdown from "../Frame/Dropdown/AccountDropdown";
 
 function Navbartop() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
-    const [dropdown, setDropdown] = useState(false);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const [dropdown, setDropdown] = useState(false);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const onMouseEnter = () => {
-        if (window.innerWidth < 960) {
-            setDropdown(false);
-        } else {
-            setDropdown(true);
-        }
-    };
-
-    const onMouseLeave = () => {
-        if (window.innerWidth < 960) {
-            setDropdown(false);
-        } else {
-            setDropdown(false);
-        }
-    };
-
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
-
-    useEffect(() => {
-        showButton();
-    }, []);
-
-    window.addEventListener('resize', showButton);
-
-    const token = localStorage.getItem("token");
-    const user = JSON.parse(localStorage.getItem("user"));
-
-    const logoutUser = () => {
-        localStorage.clear();
-        window.location.href = "/login"
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
     }
-    return (
-        <>
-            <nav className='navbartop fixed-top'>
-                <div className='navbartop-container'>
-                    <ul className='nav-top-menu'>
-                        <li className='nav-item'>
-                            <Link to='/wishlist' className='navtop-links'><i className="fas fa-globe-americas"></i>
-                                / English / USD &nbsp;<i className="dropdown fas fa-caret-down"></i>
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/wishlist' className='navtop-links'><i className="far fa-heart"></i> &nbsp;
-                                Wishlist
-                            </Link>
-                        </li>
-                        <li
-                        className='nav-item'
-                        onMouseEnter={onMouseEnter}
-                        onMouseLeave={onMouseLeave}
-                    >
-                        <Link
-                            to='/login'
-                            className='nav-dropdown'
-                            onClick={closeMobileMenu}
-                        >
-                            <i className="fas fa-user"></i>&nbsp;Account &nbsp;<i className='fas fa-caret-down' />
-                        </Link>
-                        {dropdown && <Dropdown />}
-                    </li>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    )
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  const token = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  return (
+    <>
+      <nav className="navbartop fixed-top">
+        <div className="navbartop-container">
+          <ul className="nav-top-menu">
+            <li className="nav-item">
+              <Link to="/wishlist" className="navtop-links">
+                <i className="fas fa-globe-americas"></i>/ English / USD &nbsp;
+                <i className="dropdown fas fa-caret-down"></i>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/wishlist" className="navtop-links">
+                <i className="far fa-heart"></i> &nbsp; Wishlist
+              </Link>
+            </li>
+            <li
+              className="nav-item"
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+            >
+              <Link
+                to="/login"
+                className="nav-dropdown"
+                onClick={closeMobileMenu}
+              >
+                <i className="fas fa-user"></i>&nbsp;
+                {user.firstname ? <>{user.firstname}</> : <>Account</>} &nbsp;
+                <i className="fas fa-caret-down" />
+              </Link>
+              {dropdown && <Dropdown />}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
-export default Navbartop
+export default Navbartop;
