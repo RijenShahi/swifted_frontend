e9b429b0d100d0e0812a562bf3487b7863df2dac
RijenShahi <50471796+RijenShahi@users.noreply.github.com>
2021-09-04 19:23:46 +0545

    Contact page is redesigned.

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Footer from '../Frame/Footer'
+import home from '../../images/swB.png'
+import {Link} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
 
 function Contact() {
     return (
+        <Container>
         <div>
-            
-        </div>
+        <h1><b>SWIFTED DROPSHIPMENT</b></h1>
+        <h2 className="text-center">Choice of clothes. Know more about us...</h2>
+        <br/>
+        <p className="text-center">We are the ecommerce dropshipping site mainly focused in clothing. We appreciate all kinds of clothes categories like jeans, shirts, t-shirts, jackets, joggers and much more</p>
+      
+        <img src={home} className="img__sec"/>
+      </div>
+    
+      </Container>
     )
 }
 
